d9be4c96cdacd27a7d32ce257ada7a2541eea371
hudoy92 <130566450+hudoy92@users.noreply.github.com>
2023-04-28 13:20:36 +0300

    Add files via upload

---
+++ SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/ApiController.java
@@ -0,0 +1,12 @@
+package com.example.springsecurityapplication.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping
+public class ApiController {
+
+}
